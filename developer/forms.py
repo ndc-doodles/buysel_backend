b03ad65d944d4476810cedef3708d4d38fe3c475
N-meapp <developersnme@gmail.com>
2025-05-03 15:55:37 +0530

    icon changed

--- developer/forms.py
+++ developer/forms.py
@@ -1,5 +1,16 @@
 from django import forms
 
 class SuperuserLoginForm(forms.Form):
-    username = forms.CharField(max_length=150)
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(
+        max_length=150,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Enter username',
+            'style': 'text-align: center;'
+        })
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Enter password',
+            'style': 'text-align: center;'
+        })
+    )
