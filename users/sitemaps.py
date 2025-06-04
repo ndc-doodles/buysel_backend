777f343c4ee68adff3a30e6569a88f307f5b9803
N-meapp <developersnme@gmail.com>
2025-06-04 12:43:53 +0530

    corrected

--- users/sitemaps.py
+++ users/sitemaps.py
@@ -1,24 +1,22 @@
-from django.contrib.sitemaps import Sitemap
-from django.urls import reverse
-
 class StaticViewSitemap(Sitemap):
     changefreq = "weekly"
 
     priority_mapping = {
-        'home': 1.0,
-        'about': 0.8,
-        'register': 0.8,
-        'blog': 0.8,
+        'index': 1.0,         # Corrected from 'home'
         'faq': 0.8,
-        'services': 0.8,
-        'contact': 0.8,
+        'blog': 0.8,
+        'agents': 0.8,
+        'more': 0.8,
+        'properties': 0.8,
+        'agent_form': 0.8,
+        'property_form': 0.8,
     }
 
     def items(self):
-        return list(self.priority_mapping.keys())
+        return self.priority_mapping.keys()
 
     def location(self, item):
         return reverse(item)
 
-    def priority(self, item):    
+    def priority(self, item):
         return self.priority_mapping[item]
